6e4f49c2c34bbfaec5028111e966be71aefef5f9
twotimesspicy <107835010+twotimesspicy@users.noreply.github.com>
2022-06-25 01:59:54 -0700

    Add express-async-errors library to remove try/catch

--- backend/controllers/tickets.js
+++ backend/controllers/tickets.js
@@ -7,48 +7,25 @@ ticketsRouter.get('/', async (req, res) => {
 })
 
 ticketsRouter.get('/:id', async (req, res, next) => {
-//   Ticket.findById(req.params.id)
-//     .then(ticket => {
-//       if (ticket) {
-//         res.json(ticket)
-//       } else {
-//         res.status(404).end()
-//       }
-//     })
-//     .catch(error => next(error))
-    try {
-        const ticket = await Ticket.findById(req.params.id)
-        ticket ? res.json(ticket) : res.status(404).end()
-    } catch (error) {
-        next(error)
-    }
+    const ticket = await Ticket.findById(req.params.id)
+    ticket ? res.json(ticket) : res.status(404).end()
 })
 
 ticketsRouter.post('/', async (req, res, next) => {
     const body = req.body
-
     const ticket = new Ticket({
         app: body.app,
         priority: body.priority || 'L',
         description: body.description
     })
-    try {
-        const savedTicket = await ticket.save()
-        res.json(savedTicket)
-    } catch (error) {
-        next(error)
-    }
+
+    const savedTicket = await ticket.save()
+    res.json(savedTicket)
 })
 
 ticketsRouter.delete('/:id', async (req, res, next) => {
-    try {
-        await Ticket.findByIdAndRemove(req.params.id)
-        res.status(204).end()
-    }
-    catch {
-        next(error)
-    }
-
+    await Ticket.findByIdAndRemove(req.params.id)
+    res.status(204).end()
 })
 
 ticketsRouter.put('/:id', async (req, res, next) => {
@@ -59,13 +36,8 @@ ticketsRouter.put('/:id', async (req, res, next) => {
         description: body.description
     }
 
-    try {
-        const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, ticket, { new: true })
-        res.json(updatedTicket)
-    }
-    catch {
-        next(error)
-    }
+    const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, ticket, { new: true })
+    res.json(updatedTicket)
 })
 
 module.exports = ticketsRouter
